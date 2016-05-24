98e07d7eb10fa617a4f8877e8688835a80384a5e
Umang Shah <shahumangr@gmail.com>
2016-05-24 16:08:08 -0700

    HackerRank: Introduction -> Welcome to Java!

---
+++ src/hackerrank/WelcomeToJava.java
@@ -0,0 +1,15 @@
+package hackerrank;
+
+/*
+ * HackerRank: Introduction: Welcome to Java!
+ * Description: Print "Hello World." and "Hello Java." 
+ * in two separate lines
+ */
+public class WelcomeToJava {
+
+	public static void main(String []argv)
+	{
+		System.out.println("Hello World.");
+		System.out.println("Hello Java.");
+	}
+}
